1b301aa9b05f37f01961c528ad2ae0173f0224e4
Karolaine Brandelero <55720260+bkarln@users.noreply.github.com>
2025-09-05 15:48:59 +0000

    adjust run app

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ const isProd = process.env.NODE_ENV === "production";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "",
-  base: "/astro-launch-ui/",
+  site: "http://localhost:4321/",
+  base: "/",
 });
